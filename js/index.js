b6e93ebde2e091530dc4dd560edcb4349f537063
Roy-Weiss <67227056+Roy-Weiss@users.noreply.github.com>
2020-09-05 14:02:34 -0400

    mouseover, mouseleave, click

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
 // Your code goes here
 
-const bus = document.querySelector(".intro");
+const bus = document.querySelector(".intro")
 bus.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = "purple";
-});
+    event.target.style.backgroundColor = "purple"
+})
+
+const bigBus = document.querySelector(".intro img")
+bigBus.addEventListener("mouseenter", () => {
+    bigBus.style.transform = "scale(2)"
+    bigBus.style.transition = "transform 1s"
+})
+
+
+bigBus.addEventListener("mouseleave", () => {
+    bigBus.style.transform = "scale(1)"
+    bigBus.style.transition = "transform 1s"
+})
+
+const cool = document.querySelector(".nav")
+cool.addEventListener("click", (event) => {
+    event.target.style.backgroundColor = "purple"
+})
+
+
+
+
+
+
 
